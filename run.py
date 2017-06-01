6bbc566c2ba8eb4f2a815182217f9da68ac25fa5
Joseph Zhong <joseph.zhong@hotmail.com>
2017-05-31 20:56:39 -0700

    progress

--- run.py
+++ run.py
@@ -17,16 +17,17 @@ import itertools
 
 palette = itertools.cycle(sns.color_palette())
 
-SCALING_FACTOR = 2
-UPPER_BOUND = 4
+AVERAGING_FACTOR = 5
+SCALING_FACTOR = 1.25
+UPPER_BOUND = 11
 BASE_ITER = 256
 BASE_RES = 2048
-MAX_RES = 23150
+MAX_RES = 20000
 
 EXPERIMENTS = {"res" : BASE_RES, "iter" : BASE_ITER}
 CUDA_KERNELS = ["cudaNaive", "cudaDP"]
-# KERNELS = ["naive"] + CUDA_KERNELS
-KERNELS = CUDA_KERNELS
+KERNELS = ["naive"] + CUDA_KERNELS
+# KERNELS = CUDA_KERNELS
 EXP_DIR = "experiments"
 OUT_DIR = "output"
 BASE_CMD = "./main {} {} {} {} {}"
@@ -44,10 +45,10 @@ for exp in EXPERIMENTS:
         if kernel not in experiments:
             experiments[exp][kernel] = {}
         for i in range(UPPER_BOUND):
-            val = EXPERIMENTS[exp] * SCALING_FACTOR ** i
+            val = int(EXPERIMENTS[exp] * SCALING_FACTOR ** i)
             val = min(val, MAX_RES)
             if val not in experiments[exp][kernel]:
-                experiments[exp][kernel][val] = 0
+                experiments[exp][kernel][val] = []
             output = os.path.join(OUT_DIR,
                     "kernel_{}_exp_{}_val_{}_mandelbrot_set.png".format(kernel, exp, val))
             if exp == "res":
@@ -56,17 +57,12 @@ for exp in EXPERIMENTS:
                 cmd = BASE_CMD.format(BASE_RES, BASE_RES, val, kernel, output).split()
             print "[Run] [exp='{}'] [kernel='{}'] [val='{}']".format(exp,
                     kernel, val)
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            processes.append((p, kernel, exp, val))
-
-while processes:
-    p, kernel, exp, val = processes.pop()
-    p.wait()
-    seconds = p.stdout.read()
-    os.sys.stdout.flush()
-    print "[Process] [exp='{}'] [kernel='{}'] [val='{}'] [seconds='{}']".format(exp,
-                    kernel, val, seconds)
-    experiments[exp][kernel][val] = seconds
+            for trial in range(AVERAGING_FACTOR):
+                seconds = subprocess.check_output(cmd)
+                print "[Process] [exp='{}'] [kernel='{}'] [val='{}'] [seconds='{}']".format(exp,
+                                kernel, val, seconds)
+                experiments[exp][kernel][val].append(seconds)
+            experiments[exp][kernel][val] = sum(float(second) for second in experiments[exp][kernel][val]) / AVERAGING_FACTOR
 
 # REVIEW josephz: This can be abstracted by a function of keys and vals.
 print "[Graphing Running Time]"
@@ -75,14 +71,13 @@ for exp in experiments:
     plt.title('Running Time')
     plt.xlabel(exp)
     plt.ylabel('Time(s)')
-    leg = plt.legend(frameon=True, loc="upper left")
-    leg.get_frame().set_edgecolor('b')
     for kernel in KERNELS:
         plt.scatter(experiments[exp][kernel].keys(),
                 [experiments[exp][kernel][val]
                     for val in experiments[exp][kernel]], label=kernel, color=next(palette))
+    leg = plt.legend(frameon=True, loc="upper left")
     plt.savefig(os.path.join(EXP_DIR,
-        "kernel_{}_exp_{}_val_{}_graph.png".format(kernel, exp, val)))
+        "running_time_graph_exp_{}.png".format(exp)))
     plt.show()
 
 print "[Graphing M. Pixels per second]"
@@ -91,18 +86,18 @@ for exp in experiments:
     plt.title('M. Pixels per second')
     plt.xlabel(exp)
     plt.ylabel('MPix/s')
-    leg = plt.legend(frameon=True, loc="upper left")
 
     vals = experiments[exp][kernel].keys()
     if exp == "res":
-        resolutions = [experiments[exp][kernel][val] ** 2 for val in vals]
+        resolutions = [float(val) ** 2 / 1000000 / experiments[exp][kernel][val] for val in vals]
     else:
-        resolutions = [BASE_RES ** 2 for val in vals]
+        resolutions = [BASE_RES ** 2 / 1000000 / experiments[exp][kernel][val] for val in vals]
 
     for kernel in KERNELS:
         plt.scatter(vals, resolutions, label=kernel, color=next(palette))
+    leg = plt.legend(frameon=True, loc="upper left")
     plt.savefig(os.path.join(EXP_DIR,
-        "kernel_{}_exp_{}_val_{}_graph.png".format(kernel, exp, val)))
+        "mpps_graph_exp_{}.png".format(exp)))
     plt.show()
 
 
